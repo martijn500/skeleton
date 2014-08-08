b7cf2e9966daa56a13a1b89febeb34eb1db536dc
MWIJST <contact@martijnvanderwijst.nl>
2014-08-08 11:45:09 +0200

    skeleton commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name skeletonApp
+ * @description
+ * # skeletonApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('skeletonApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
